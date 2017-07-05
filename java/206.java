ea03a2f623cd8ae833b2a5f675fbc4c309d91103
LOLha <710514154@qq.com>
2017-07-05 22:14:53 +0800

    Update 206.java

--- java/206.java
+++ java/206.java
@@ -10,8 +10,20 @@ public class S206 {
 	        head.next = null;
 	        p.next = head;
 	        return n;
-	        
-	        
-	    }
+	 }
+	 public ListNode reverseList(ListNode head){
+	 	if(head==null||head.next==null) return head;
+		ListNode pre = null;
+		ListNode next = null;
+		ListNode cur = head;
+		while(cur!=null){
+		    next = cur.next;
+		    cur.next = pre;
+	            pre = cur;
+		    cur = next;
+		}
+	        return pre;
+	 }
+	
 
 }
