018607f90df60ba3fc00126377034eb0a7d6cd11
LOLha <710514154@qq.com>
2017-07-26 21:50:34 +0800

    Create 98.java

---
+++ java/98.java
@@ -0,0 +1,11 @@
+public class S98{
+    TreeNode pre = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root==null) return true;
+        if(!isValidBST(root.left)) return false;
+        if(pre!=null&&pre.val>=root.val) return false;
+        pre = root;
+        if(!isValidBST(root.right)) return false;
+        return true;
+    }
+}
